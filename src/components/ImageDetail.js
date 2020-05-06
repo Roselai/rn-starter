a3f5cfd6802896eb9981efc03e9a2ea1e3010210
Shukti Shaikh <shukti.azad@gmail.com>
2020-05-06 19:50:17 -0400

    make score uppercase

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,14 +1,14 @@
 import React from "react";
-import {View, Text, StyleSheet, Image} from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const ImageDetail = ({title, imageSource, imageScore}) => {
-	return (
-		<View>
-			<Image source={imageSource} />
-			<Text>{title}</Text>
-			<Text>Image score - {imageScore}</Text>
-		</View>
-	);
+const ImageDetail = ({ title, imageSource, imageScore }) => {
+  return (
+    <View>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>Image Score - {imageScore}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
