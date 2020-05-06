2d2159a86e893a21e4d64122e9e26b006d10fed0
Shukti Shaikh <shukti.azad@gmail.com>
2020-05-06 19:48:21 -0400

    add image score prop

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,24 +1,27 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
-  return (
-    <View>
-      <ImageDetail
-        title="Forest"
-        imageSource={require("../../assets/forest.jpg")}
-      />
-      <ImageDetail
-        title="Beach"
-        imageSource={require("../../assets/beach.jpg")}
-      />
-      <ImageDetail
-        title="Mountain"
-        imageSource={require("../../assets/mountain.jpg")}
-      />
-    </View>
-  );
+	return (
+		<View>
+			<ImageDetail
+				title="Forest"
+				imageSource={require("../../assets/forest.jpg")}
+				imageScore={9}
+			/>
+			<ImageDetail
+				title="Beach"
+				imageSource={require("../../assets/beach.jpg")}
+				imageScore={7}
+			/>
+			<ImageDetail
+				title="Mountain"
+				imageSource={require("../../assets/mountain.jpg")}
+				imageScore={4}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({});
