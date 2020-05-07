0d6654d9da602aa86d51e838c87655092ebd3c6d
Shukti Shaikh <shukti.azad@gmail.com>
2020-05-07 10:53:58 -0400

    add flatlist to display randomly generated colors

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,27 +1,35 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Button } from "react-native";
+import React, {useState} from "react";
+import {View, StyleSheet, Button, FlatList} from "react-native";
 
 const ColorScreen = () => {
-  const [colors, setColors] = useState([]);
+	const [colors, setColors] = useState([]);
 
-  return (
-    <View>
-      <Button
-        title="Add a Color"
-        onPress={() => {
-          setColors([...colors, randomRGB()]);
-        }}
-      />
-      <View style={{ height: 100, width: 100, backgroundColor: randomRGB() }} />
-    </View>
-  );
+	return (
+		<View>
+			<Button
+				title="Add a Color"
+				onPress={() => {
+					setColors([...colors, randomRGB()]);
+				}}
+			/>
+			<FlatList
+				keyExtractor={item => item}
+				data={colors}
+				renderItem={({item}) => {
+					return (
+						<View style={{height: 100, width: 100, backgroundColor: item}} />
+					);
+				}}
+			/>
+		</View>
+	);
 };
 
 const randomRGB = () => {
-  const red = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 256);
-  const blue = Math.floor(Math.random() * 256);
-  return `rgb(${red},${green},${blue})`;
+	const red = Math.floor(Math.random() * 256);
+	const green = Math.floor(Math.random() * 256);
+	const blue = Math.floor(Math.random() * 256);
+	return `rgb(${red},${green},${blue})`;
 };
 
 const styles = StyleSheet.create({});
