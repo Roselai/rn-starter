26d1e9712540f43de66846f0565ae8f23ed64cf1
Shukti Shaikh <shukti.azad@gmail.com>
2020-05-07 11:56:50 -0400

    pass callback functions to color counter

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import {View, Text, StyleSheet, Button} from "react-native";
 
-const ColorCounter = ({ color }) => {
-  return (
-    <View>
-      <Text>{color}</Text>
-      <Button title={`Increase ${color}`} />
-      <Button title={`Decrease ${color}`} />
-    </View>
-  );
+const ColorCounter = ({color, onIncrease, onDecrease}) => {
+	return (
+		<View>
+			<Text>{color}</Text>
+			<Button title={`Increase ${color}`} onPress={() => onIncrease()} />
+			<Button title={`Decrease ${color}`} onPress={() => onDecrease()} />
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({});
