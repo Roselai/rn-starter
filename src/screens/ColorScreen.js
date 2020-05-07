5220fd9ec99e3962a727b249bf057b26c27adb4c
Shukti Shaikh <shukti.azad@gmail.com>
2020-05-06 21:01:11 -0400

    add button

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, {useState} from "react";
+import {View, StyleSheet, Button} from "react-native";
 
 const ColorScreen = () => {
-  return (
-    <View>
-      <Text>Add color</Text>
-    </View>
-  );
+	return (
+		<View>
+			<Button title="Add a Color" />
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({});
